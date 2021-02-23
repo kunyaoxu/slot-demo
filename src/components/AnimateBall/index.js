8f10baa905a6f912aee0fc17d229bf72533276b7
kyan.hsu <kyan.hsu@yjtech.tw>
2021-02-23 11:29:17 +0800

    feat: 實現緩慢停止滾動

--- src/components/AnimateBall/index.js
+++ src/components/AnimateBall/index.js
@@ -1,4 +1,4 @@
-import { useMemo, useState } from 'react';
+import { useEffect, useState } from 'react';
 import {
   BallsWrapper,
   BallsWrapperWithAnimation,
@@ -6,24 +6,40 @@ import {
   Ball,
 } from './style';
 
+const STOP = 0;
+const RUN = 1;
+const RUN_SLOW = 2;
+
 const AnimateBall = ({ value }) => {
-  const [isPlay, setIsPlay] = useState(false);
-  const translateInfo = useMemo(() => {
+  const [playState, setPlayState] = useState(STOP);
+  const [preValue, setPreValue] = useState(0);
+
+  useEffect(() => {
     if (value !== null) {
-      setIsPlay(true);
+      setPlayState(RUN);
     }
-    return `translateY(${(value * -100) / 21}%`;
   }, [value]);
 
   return (
     <Wrapper>
       <BallsWrapperWithAnimation
-        className={isPlay ? 'play' : ''}
-        onAnimationEnd={() => setIsPlay(false)}
+        className={playState === RUN ? 'play' : ''}
+        onAnimationEnd={() => {
+          setPlayState(RUN_SLOW);
+          setPreValue(value + 10);
+        }}
+        onAnimationIteration={() => {
+          setPreValue(value);
+        }}
       >
         <BallsWrapper
+          className={playState === RUN_SLOW ? 'play' : ''}
+          onTransitionEnd={() => {
+            setPlayState(STOP);
+            setPreValue(value);
+          }}
           style={{
-            transform: translateInfo,
+            transform: `translateY(${(preValue * -100) / 21}%`,
           }}
         >
           <Ball>0</Ball>
